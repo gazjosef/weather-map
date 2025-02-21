ed3ef961c4209c031b5fb3a287b7c7932d65adee
Gareth <garethjhind@gmail.com>
2025-02-21 13:47:14 +1100

    Fix: Map Working

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,20 +1,29 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import { useCity } from "../context/CityContext";
-// import { LatLngExpression } from "leaflet";
 import styled from "styled-components";
+import { useEffect } from "react";
 
 const MapWrapper = styled.div`
-  position: absolute;
+  position: relative;
   top: 0;
   left: 0;
-  // width: 100%;
-  width: calc(100vw - 250px); /* Adjust width to account for Sidebar */
+  width: calc(100vw - 150px); /* Adjust width to account for Sidebar */
   height: 100vh;
-  background: red; /* TEMPORARY - Remove once confirmed */
 `;
 
-// const center: LatLngExpression = [-33.8688, 151.2093]; // Explicit type for Leaflet coordinates
+const UpdateMapCenter = () => {
+  const { selectedCity } = useCity();
+  const map = useMap();
+
+  useEffect(() => {
+    if (selectedCity) {
+      map.setView([selectedCity.lat, selectedCity.lon], 10);
+    }
+  }, [selectedCity, map]);
+
+  return null;
+};
 
 const Map = () => {
   const { selectedCity } = useCity();
@@ -29,12 +38,14 @@ const Map = () => {
             : [-33.8688, 151.2093]
         }
         zoom={10}
-        style={{ width: "100%", height: "100%", border: "5px solid blue" }}
+        style={{ width: "100%", height: "100%" }}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
+
+        <UpdateMapCenter />
       </MapContainer>
     </MapWrapper>
   );
